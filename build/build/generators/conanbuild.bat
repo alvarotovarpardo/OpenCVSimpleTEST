6ce6a15fa54095af15d3ac28d942d25f7e781be1
Alvaro Tovar <atovar@sensia-solutions.com>
2025-01-17 12:51:24 +0100

    second commit

--- build/build/generators/conanbuild.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "%~dp0/conanautotoolstoolchain.bat"
-call "%~dp0/conanvcvars.bat"
-call "%~dp0/conanbuildenv-release-x86_64.bat"
--- build/build/generators/conandeps_legacy.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(aravis)
-find_package(OpenCV)
-find_package(doctest)
-find_package(libmodbus)
-
-set(CONANDEPS_LEGACY  aravis::aravis  opencv::opencv  doctest::doctest  libmodbus::libmodbus )
